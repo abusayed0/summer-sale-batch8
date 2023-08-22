c1e7dbf6ee91eae750485ab5d9fea0146f9b2082
abusayed0 <abusayed48876@gmail.com>
2023-08-22 19:19:02 +0600

    added cards, cart and calculation functionality

---
+++ js/index.js
@@ -0,0 +1,59 @@
+console.log("js/index.js");
+
+document.getElementById("product1").addEventListener("click", function () {
+    addToCarts("product1-price", "product1-name");
+});
+
+document.getElementById("product2").addEventListener("click", function () {
+    addToCarts("product2-price", "product2-name");
+});
+
+document.getElementById("product3").addEventListener("click", function () {
+    addToCarts("product3-price", "product3-name");
+});
+
+document.getElementById("product4").addEventListener("click", function () {
+    addToCarts("product4-price", "product4-name");
+});
+
+document.getElementById("product5").addEventListener("click", function () {
+    addToCarts("product5-price", "product5-name");
+});
+
+document.getElementById("product6").addEventListener("click", function () {
+    addToCarts("product6-price", "product6-name");
+});
+document.getElementById("product7").addEventListener("click", function () {
+    addToCarts("product7-price", "product7-name");
+});
+document.getElementById("product8").addEventListener("click", function () {
+    addToCarts("product8-price", "product8-name");
+});
+document.getElementById("product9").addEventListener("click", function () {
+    addToCarts("product9-price", "product9-name");
+});
+
+document.getElementById("btn-coupon-apply").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const availableCoupon = "SELL200";
+
+    const couponField = document.getElementById("coupon-field");
+    const userCoupon = couponField.value;
+    couponField.value = "";
+    
+    if (userCoupon == availableCoupon) {
+        isDiscountAdded = true;
+        discountCalulationAndUpdate(true);
+    }
+    else{
+        discountCalulationAndUpdate(false);
+        isDiscountAdded = false;
+        alert("Invalid coupon.");
+    }
+
+});
+
+document.getElementById("btn-make-purchase").addEventListener("click", function(e){
+    console.log(e.target);
+});
---
+++ js/utilities.js
@@ -0,0 +1,60 @@
+console.log("js/utilites.js");
+let isDiscountAdded = false;
+function getElementInnerTextById(elementId){
+    const element = document.getElementById(elementId);
+    const elementInnerTextStr = element.innerText;
+    const elementInnerTextNum = parseFloat(elementInnerTextStr);
+    return elementInnerTextNum;
+}
+
+function setElementInnerTextById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value.toFixed(2);
+}
+
+function makeBtnEnable(btnId){
+    const btn = document.getElementById(btnId);
+    btn.removeAttribute("disabled");
+    btn.style.backgroundColor = "#E527B2";
+}
+
+function discountCalulationAndUpdate(addDiscount){
+    const totalPrice = getElementInnerTextById("total-price");
+    if(addDiscount){
+        const discount = totalPrice * 0.2;
+        setElementInnerTextById("discount-element", discount);
+
+        const newDiscoutTotalPrice =  totalPrice - discount;
+        setElementInnerTextById("discount-total-price", newDiscoutTotalPrice)
+    }
+    else{
+        setElementInnerTextById("discount-element", 0);
+        setElementInnerTextById("discount-total-price", totalPrice);
+       
+    }
+}
+
+function addToCarts(productPriceId, productNameElementId){
+    const previousTotalPrice = getElementInnerTextById("total-price");
+    const productPrice =getElementInnerTextById(productPriceId);
+    const newTotalPrice = previousTotalPrice + productPrice;
+    setElementInnerTextById("total-price", newTotalPrice);
+    
+    newTotalPrice >= 200 && makeBtnEnable("btn-coupon-apply");
+    newTotalPrice > 0 && makeBtnEnable("btn-make-purchase");
+
+    // extra
+    
+    isDiscountAdded ? discountCalulationAndUpdate(true) : discountCalulationAndUpdate(false);
+
+    const productName= document.getElementById(productNameElementId).innerText;
+    
+    const addedProductsContainer = document.getElementById("added-products-container");
+    const li = document.createElement("li");
+    li.innerText = productName;
+    li.classList.add("text-xl","font-medium");
+    addedProductsContainer.appendChild(li);
+
+    // extra 
+    document.getElementById("total-added").innerText = document.getElementById("added-products-container").childElementCount;
+}
